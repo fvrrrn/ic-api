98caaee3a16eaa17ca8fa645c72bd8237033f2b0
Boris <boris.chernystrand@gmail.com>
2020-05-28 16:50:54 +0300

    body-paser added

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ const helmet = require('helmet')
 const cors = require('cors')
 const fs = require('fs')
 const { logger } = require('./lib/logger')
+const bodyParser = require('body-parser')
 
 const app = express()
 app.use(helmet())
 app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 const isProduction = process.env.NODE_ENV !== 'development'
 
@@ -56,21 +59,24 @@ if (!isProduction) {
   })
 }
 
+// .к. nginx сам по себе пропускает только https,
+// десь необязательно его включать, поэтому
+// rocess.env.NODE_ENV === 'development' изменено на true
+
 // http
-if (process.env.NODE_ENV === 'development') {
+if (true) {
   let server = app.listen(process.env.PORT || 3000, function () {
-    console.log('Listening on port ' + server.address().port)
+    console.log('HTTP: Listening on port ' + server.address().port)
   })
 } else {
   const sslOptions = {
-    key: fs.readFileSync('/etc/pki/nginx/private/server.key'),
-    cert: fs.readFileSync('/etc/pki/nginx/server.cer'),
+    key: readFileSync('/etc/ssl/uni/private.key'),
+    cert: readFileSync('/etc/ssl/uni/cert.crt'),
   }
 
-  https.createServer(sslOptions, app).listen(process.env.PORT || 3000, () => {
-    console.log(`Listening on ${process.env.PORT}`)
+  createServer(sslOptions, app).listen(process.env.PORT || 3000, () => {
+    console.log(`HTTPS: Listening on ${process.env.PORT}`)
   })
 }
-
 module.exports = router
 module.exports = app
